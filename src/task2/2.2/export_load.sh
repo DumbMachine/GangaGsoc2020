ce73c40d4ab2539bc0ed954bcd30427bbdf1a11d
dumbmachine <ratin.kumar.2k@gmail.com>
2020-03-09 02:58:44 +0530

    writing the PROJECT.md

--- src/task2/2.2/export_load.sh
+++ src/task2/2.2/export_load.sh
@@ -4,5 +4,5 @@ sudo docker stop postgres; sudo docker rm postgres;
 sudo docker run --name postgres -p 5432:5432 -e POSTGRES_PASSWORD=ganga -d postgres
 sudo docker run --name mongo -p 27017:27017 -d mongo;
 
-ganga export_load_method_postgres.py
-ganga export_load_method.py
+ganga export_load.py
+ganga export_load_postgres.py
--- src/task2/2.2/full_print.sh
+++ src/task2/2.2/full_print.sh
@@ -4,5 +4,5 @@ sudo docker stop postgres; sudo docker rm postgres;
 sudo docker run --name postgres -p 5432:5432 -e POSTGRES_PASSWORD=ganga -d postgres
 sudo docker run --name mongo -p 27017:27017 -d mongo;
 
-ganga full_print_method.py
-ganga full_print_method_postgres.py
+ganga full_print.py
+ganga full_print_postgres.py
